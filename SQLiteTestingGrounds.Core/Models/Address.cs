b498ef8cdd4e97a8ab43bddd28caffab96a2000e
blairacuda <illuminatisucks@gmail.com>
2015-04-16 22:54:56 -0700

    Initial Commit

---
+++ SQLiteTestingGrounds.Core/Models/Address.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SQLiteTestingGrounds.Core.Models
+{
+    public class Address
+    {
+        public int Id { get; set; }
+        public string CityName { get; set; }
+        public string State { get; set; }
+        public string StreetAddress1 { get; set; }
+        public string StreetAddress2 { get; set; }
+        public string ZipCode { get; set; }
+    }
+}
